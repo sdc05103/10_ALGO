93df4087d1e49150dcdab80569bc8557342a6686
ChaeyoonWoo <heeni1004@gmail.com>
2024-10-21 20:46:01 +0900

    음료수 얼려 먹기 풀이 추가

---
+++ etc/음료수 얼려 먹기.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = []
+
+for i in range(N):
+    graph.append(list(map(int, input().strip())))
+
+def dfs(x, y):
+    if x <= -1 or x >= N or y <= -1 or y >= M:
+        return False
+    
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    
+    return False
+    
+result = 0
+for i in range(N):
+    for j in range(M):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
+
+    
+
